764753037dd25dfd2bf4a7d1175895f78b65c4e1
sabdahtb <sabdahutabarat2804@gmail.com>
2022-10-24 14:31:18 +0700

    lesson 10 override fun

--- src/kotlin/app/inheritance.kt
+++ src/kotlin/app/inheritance.kt
@@ -1,10 +1,16 @@
 package app
 
 import data.Manager
+import data.President
 
 fun main() {
     val manager = Manager("kyle")
 //    .sayHello is from Manager parent
     manager.sayHello("Brock")
-//    name = Brock, this.name = kyle
+//    name = Brock, this.name = kyle [PRINTLN]
+
+//    Call an override function
+    val president = President("kite")
+    president.sayHello("ken")
+//    Hello my name is ken and i am override function child of kite [PRINTLN]
 }
--- src/kotlin/data/Employee.kt
+++ src/kotlin/data/Employee.kt
@@ -6,8 +6,19 @@ open class Employee(private val name: String) {
     }
 }
 
+open class NewEmployee(val name: String) {
+    open fun sayHello(name: String) {
+        println("name = $name, this.name = ${this.name}")
+    }
+}
+
 // inheritance
 // Employee = Parent
 class Manager(name: String) : Employee(name) {}
 
-class President(name: String) : Employee(name) {}
+class President(name: String) : NewEmployee(name) {
+// Open fun is can make to override fun
+    override fun sayHello(name: String) {
+        println("Hello my name is $name and i am override function child of ${this.name}")
+    }
+}
