be7c308ddc7a1210c9ac60723f2084043ed6db38
sabdahtb <sabdahutabarat2804@gmail.com>
2022-10-24 11:42:18 +0700

    lesson 4 second constructor

--- src/kotlin/app/object.kt
+++ src/kotlin/app/object.kt
@@ -4,7 +4,7 @@ import data.Car
 import data.Person
 
 fun main() {
-    // Call object Properties
+//    Call object Properties
     val eko = Person()
     eko.age = 17
 
@@ -12,11 +12,16 @@ fun main() {
 //    initializer block has been called [PRINTLN]
 //    Eko is 17 and true [PRINTLN]
 
-    // call object with constructors
+//     call object with constructors
     val bmw = Car("BMW", 10)
-    val mercy = Car("Mercy")
 
     println("The ${bmw.carName} is ${bmw.year} years old")
-    // The  is 0 years old [PRINTLN]
+//    The  is 0 years old [PRINTLN]
+
+//    call a second constructor
+    val toyota = Car("My car")
+    println(toyota.carName)
+//    Secondary constructors 2
+//    My car
 
 }
--- src/kotlin/data/Car.kt
+++ src/kotlin/data/Car.kt
@@ -1,7 +1,17 @@
 package data
 
 // constructor
-class Car(carName: String, year: Int? = 10 ) {
-    val carName: String = ""
-    var year: Int? = 0
+class Car(carName: String, year: Int? = 10, name: String? = "" ) {
+
+    // create second constructor
+    constructor(carName: String, name: String): this(carName, null, name) {
+        println("Secondary constructors 1")
+    }
+    constructor(carName: String): this(carName, null,  null) {
+        println("Secondary constructors 2")
+    }
+
+    val carName: String = carName
+    var year: Int? = year
+    var name: String? = name
 }
